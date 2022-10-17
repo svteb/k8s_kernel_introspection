be249e51583f4df1e02ad0eefa46b65d8fa91cd3
Akash Manohar <akash@akash.im>
2022-10-17 09:55:23 -0400

    Fix status_by_proc test
    Signed-off-by: Akash Manohar <akash@akash.im>

--- spec/kernel_introspection_spec.cr
+++ spec/kernel_introspection_spec.cr
@@ -1,24 +1,28 @@
-require "../spec_helper"
-require "airgap"
+require "./spec_helper"
 require "kubectl_client"
-require "../../src/tasks/utils/kernel_instrospection.cr"
+require "./../src/kernel_introspection.cr"
 require "file_utils"
-require "sam"
 
 describe "KernelInstrospection" do
+  before_all do
+    ClusterTools.install
+  end
 
   it "'#status_by_proc' should return all statuses for all containers in a pod", tags: ["kernel-introspection"] do
-    pods = KubectlClient::Get.pods_by_label(pods, "name", "cri-tools")
+    result = KubectlClient::ShellCmd.run("kubectl run nginx --image=nginx --labels='name=nginx'", "kubectl_run", force_output=true)
+    pods = KubectlClient::Get.pods_by_nodes(KubectlClient::Get.schedulable_nodes_list)
+    pods.should_not be_nil
+    pods = KubectlClient::Get.pods_by_label(pods, "name", "nginx")
     pods.should_not be_nil
 
-    KubectlClient::Get.resource_wait_for_install("Daemonset", "cri-tools")
+    KubectlClient::Get.resource_wait_for_install("pod", "nginx")
     pods.size.should be > 0
     first_node = pods[0]
-    statuses = KernelIntrospection::K8s.status_by_proc(first_node.dig("metadata", "name"), "cri-tools")
-    LOGGING.info "statuses: #{statuses}"
+    statuses = KernelIntrospection::K8s.status_by_proc(first_node.dig("metadata", "name"), "nginx")
+    Log.info { "process-statuses: #{statuses}" }
     (statuses).should_not be_nil
 
-    (statuses.find{|x| x["cmdline"]=="sleep\u0000infinity\u0000"} ).should_not be_nil
+    (statuses.find{|x| x["cmdline"].includes?("nginx: master process")} ).should_not be_nil
   end
 
   # it "'#find_first_process' should return all statuses for all containers in a pod", tags: ["kernel-introspection"]  do
@@ -33,7 +37,7 @@ describe "KernelInstrospection" do
   #     LOGGING.info `./cnf-testsuite cnf_cleanup cnf-path=sample-cnfs/sample_coredns`
   #     $?.success?.should be_true
   #   end
-  end
+  # end
 
 end
 
