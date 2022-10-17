80c9c759c52848b4f43a97068b2079d12171cec8
Akash Manohar <akash@akash.im>
2022-10-17 19:35:01 +0530

    Spec for find_first_process
    Signed-off-by: Akash Manohar <akash@akash.im>

--- spec/kernel_introspection_spec.cr
+++ spec/kernel_introspection_spec.cr
@@ -9,7 +9,7 @@ describe "KernelInstrospection" do
   end
 
   it "'#status_by_proc' should return all statuses for all containers in a pod", tags: ["kernel-introspection"] do
-    result = KubectlClient::ShellCmd.run("kubectl run nginx --image=nginx --labels='name=nginx'", "kubectl_run", force_output=true)
+    result = KubectlClient::ShellCmd.run("kubectl run nginx --image=nginx --labels='name=nginx'", "kubectl_run_nginx", force_output=true)
     pods = KubectlClient::Get.pods_by_nodes(KubectlClient::Get.schedulable_nodes_list)
     pods.should_not be_nil
     pods = KubectlClient::Get.pods_by_label(pods, "name", "nginx")
@@ -23,21 +23,20 @@ describe "KernelInstrospection" do
     (statuses).should_not be_nil
 
     (statuses.find{|x| x["cmdline"].includes?("nginx: master process")} ).should_not be_nil
+
+    KubectlClient::Delete.command("pod/nginx", "kubectl_delete_nginx", force_output=true)
   end
 
-  # it "'#find_first_process' should return all statuses for all containers in a pod", tags: ["kernel-introspection"]  do
-  #   # KubectlClient::Apply.namespace(TESTSUITE_NAMESPACE)
-  #   begin
-  #     LOGGING.info `./cnf-testsuite cnf_setup cnf-path=sample-cnfs/sample_coredns`
-  #     # Dockerd.install
-  #     pod_info = KernelIntrospection::K8s.find_first_process("coredns")
-  #     Log.info { "pod_info: #{pod_info}"}
-  #     (pod_info).should_not be_nil
-  #   ensure
-  #     LOGGING.info `./cnf-testsuite cnf_cleanup cnf-path=sample-cnfs/sample_coredns`
-  #     $?.success?.should be_true
-  #   end
-  # end
+  it "'#find_first_process' should return all statuses for all containers in a pod", tags: ["kernel-introspection"]  do
+    result = KubectlClient::ShellCmd.run("kubectl run nginx --image=nginx --labels='name=nginx'", "kubectl_run_nginx", force_output=true)
+    begin
+      pod_info = KernelIntrospection::K8s.find_first_process("nginx: master process")
+      Log.info { "pod_info: #{pod_info}"}
+      (pod_info).should_not be_nil
+    ensure
+      KubectlClient::Delete.command("pod/nginx", "kubectl_delete_nginx", force_output=true)
+    end
+  end
 
 end
 
